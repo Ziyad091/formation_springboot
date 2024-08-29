6a6e73d55ff637a4480c18e2313ab38b845c8937
Ziyad091 <z.bougdou@polyswantech.com>
2024-08-29 12:06:55 +0200

    création classe

---
+++ formation_springboot/src/main/java/com/dev/formation_springboot/classes/Produit.java
@@ -0,0 +1,31 @@
+package com.dev.formation_springboot.classes;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Entity
+@Table(name = produit)
+public class Produit {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String nom;
+	private String categorie;
+	private int quantite;
+	private float prix;
+	private String lienimg;
+	private String description;
+	private boolean actif;
+	
+	
+}
